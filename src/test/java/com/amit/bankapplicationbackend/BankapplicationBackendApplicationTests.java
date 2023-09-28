5f715b98dcde0270ae7c003e1932566e5afab524
Amit <amitpal21ap@gmail.com>
2023-09-28 17:01:41 +0530

    initial commit

---
+++ src/test/java/com/amit/bankapplicationbackend/BankapplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.bankapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankapplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
